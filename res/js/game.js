ac52807c27910c351390df4f7aeb1fd16d88c991
ttoomas <folprecht.to@gmail.com>
2023-04-16 19:44:19 +0200

    files splitted, test start game button

---
+++ res/js/game.js
@@ -0,0 +1,44 @@
+import { shipPositions } from "./gameSetting.js";
+
+const startGameBtn = document.querySelector('.create__continueBtn');
+const createShipContainers = document.querySelectorAll('.create__shipContainer');
+const createFieldContainers = document.querySelectorAll('.create__fieldContainer');
+const eachCreateFields = document.querySelectorAll('.createField');
+
+let createFields = [];
+
+createFieldContainers.forEach(createContainer => {
+    createFields.push(createContainer.querySelectorAll('.createField'));
+})
+
+
+startGameBtn.addEventListener('click', () => {
+    console.log('Just started the game');
+    console.log(shipPositions);
+    console.log('---------------');
+
+    // Delete ships
+    createShipContainers.forEach(ship => {
+        ship.style.display = "none";
+    })
+
+    // Reset background
+    eachCreateFields.forEach(field => {
+        field.style.backgroundColor = "#0093ff45";
+    })
+
+    // Change background of ship positions
+    let allShipCoords = [];
+
+    shipPositions.forEach(shipInfo => {
+        if(!shipInfo.coords.length) return;
+
+        shipInfo.coords.map(coords => allShipCoords.push(coords));
+    })
+
+    if(!allShipCoords.length) return;
+
+    allShipCoords.forEach((coord) => {
+        createFields[coord.y][coord.x].style.backgroundColor = "orange";
+    })
+})
---
+++ res/js/gameSetting.js
@@ -0,0 +1,38 @@
+export let shipPositions = [
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "battleship"
+    },
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "carrier"
+    },
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "cruiser"
+    },
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "destroyer"
+    },
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "rescuership"
+    },
+    {
+        x: 0,
+        y: 0,
+        coords: [],
+        name: "submarine"
+    }
+];
